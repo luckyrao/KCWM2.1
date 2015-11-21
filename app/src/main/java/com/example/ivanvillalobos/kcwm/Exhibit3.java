b954c30d0da6fc611e8dc4a6bf8ba97b48bf1a26
ivanvillalobos <villa054>
2015-11-20 20:43:04 -0600

    Initial commit

---
+++ app/src/main/java/com/example/ivanvillalobos/kcwm/Exhibit3.java
@@ -0,0 +1,93 @@
+package com.example.ivanvillalobos.kcwm;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class Exhibit3 extends AppCompatActivity {
+
+
+    MediaPlayer audio;
+    int paused;
+
+    public void playMusic (View view) {
+        if(audio == null) {
+            audio = MediaPlayer.create(this, R.raw.tovivaldi);
+            audio.start();
+        }else if(!audio.isPlaying()){
+            audio.seekTo(paused);
+            audio.start();
+        }
+    }
+
+    public void pauseMusic(View view) {
+        audio.pause();
+        paused = audio.getCurrentPosition();
+    }
+
+    public void stopMusic(View view) {
+        audio.release();
+        audio = null;
+    }
+
+
+
+
+
+
+    private android.support.v7.widget.Toolbar toolbar;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_exhibits, menu);
+
+        return true;
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exhibit3);
+
+
+
+        toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+
+
+    }
+
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if(id==R.id.navigate){
+            startActivity(new Intent(this, Exhibit4.class));
+            finish();
+
+        }
+
+        if(id==R.id.navigatePrevious){
+            startActivity(new Intent(this, Exhibit2.class));
+            finish();
+
+        }
+
+        if(id==R.id.map){
+            startActivity(new Intent(this, Map.class));
+            finish();
+
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
