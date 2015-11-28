38afaa33433166c6464dd39a5c804a9d9ccae73f
luckyrao <lakuleshrao89@gmail.com>
2015-11-28 17:07:03 -0600

    Update Exhibit4b.java

--- app/src/main/java/com/example/ivanvillalobos/kcwm/Exhibit4b.java
+++ app/src/main/java/com/example/ivanvillalobos/kcwm/Exhibit4b.java
@@ -10,7 +10,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
-
+import android.widget.SeekBar;
 public class Exhibit4b extends AppCompatActivity implements GestureDetector.OnGestureListener,
         GestureDetector.OnDoubleTapListener{
 
@@ -19,7 +19,9 @@ public class Exhibit4b extends AppCompatActivity implements GestureDetector.OnGe
     private static final int SWIPE_DISTANCE_THRESHOLD = 100;
     private static final int SWIPE_VELOCITY_THRESHOLD = 100;
 
+   SeekBar seek_bar;//seekbar
 
+    Handler seekHandler = new Handler();//seekbar
     MediaPlayer audio;
     int paused;
 
@@ -64,6 +66,8 @@ public class Exhibit4b extends AppCompatActivity implements GestureDetector.OnGe
 
         this.gestureDetector= new GestureDetectorCompat(this,this);
         gestureDetector.setOnDoubleTapListener(this);
+ getInit();
+        seekUpdation();
 
         toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
@@ -72,7 +76,26 @@ public class Exhibit4b extends AppCompatActivity implements GestureDetector.OnGe
 
 
     }
+    public void getInit() {
+        seek_bar = (SeekBar) findViewById(R.id.seek_bar);
 
+
+        audio = MediaPlayer.create(this, R.raw.tovivaldi);
+        seek_bar.setMax(audio.getDuration());
+    }
+    Runnable run = new Runnable()
+    {
+        @Override
+        public void run()
+        {
+            seekUpdation();
+        }
+    };
+    public void seekUpdation()
+    {
+        seek_bar.setProgress(audio.getCurrentPosition());
+        seekHandler.postDelayed(run, 0);
+    }
 
     public boolean onOptionsItemSelected(MenuItem item) {
 
